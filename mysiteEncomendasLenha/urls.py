07bb87fcb8e7c7d8aaddec1a724c7e43af39daea
enmanuel martins <eel2000martins@hotmail.com>
2021-09-21 15:59:50 +0100

    Autenticacao com django

--- mysiteEncomendasLenha/urls.py
+++ mysiteEncomendasLenha/urls.py
@@ -20,6 +20,7 @@ from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 router = routers.DefaultRouter()
@@ -31,6 +32,7 @@ urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
