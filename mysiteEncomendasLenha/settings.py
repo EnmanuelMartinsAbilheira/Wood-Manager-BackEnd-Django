e6996fd281a9b7e87e5ea7144a29f13e50a9c05d
enmanuel martins <eel2000martins@hotmail.com>
2021-10-19 15:45:43 +0100

    Static files fix

--- mysiteEncomendasLenha/settings.py
+++ mysiteEncomendasLenha/settings.py
@@ -141,9 +141,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
